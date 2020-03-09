3e99356cdeb2aada65f76a58918057697b270a79
pankaj Vashisht <iampankajvashisht@gmail.com>
2020-03-09 11:53:53 +0530

    update data

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 const database = {
-  default: process.env.DATABASETYPE || "mysql",
-  mysql: {
-    connectionLimit: 100,
-    host: process.env.HOST || "localhost",
-    user: process.env.USERNAME || "root",
-    password: process.env.PASSWORD || "",
-    database: process.env.DATABASE || "rou",
-  },
-  url: "",
+	default: process.env.DATABASETYPE || 'mysql',
+	mysql: {
+		connectionLimit: 100,
+		host: process.env.HOST || 'localhost',
+		user: process.env.USERNAME || 'root',
+		password: process.env.PASSWORD || '',
+		database: process.env.DATABASE || ''
+	},
+	url: ''
 };
 
 module.exports = database;
